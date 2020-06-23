f3b6c83f52023c97a5df550c8a69dd0a44e2e1a8
prabesh013 <nprabesh013@gmail.com>
2020-06-23 19:44:46 +0545

    Made Exercise-2.js

---
+++ JS Exercise 2-7/Exercise-2.js
@@ -0,0 +1,15 @@
+function pattern(num) {
+  if (num === 0) {
+    return "";
+  } else {
+    var s = "";
+    for (var i = 0; i < num; i++) {
+      s = s + "*";
+    }
+    console.log(s);
+    pattern(num - 1);
+  }
+}
+
+var userInput = prompt();
+pattern(userInput);
