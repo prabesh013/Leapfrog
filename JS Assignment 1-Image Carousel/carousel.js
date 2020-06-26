b426b1a6ef2878a355fc27ecfda6acd1eb74441d
prabesh013 <nprabesh013@gmail.com>
2020-06-26 07:43:50 +0545

    Fix some bugs

--- JS Assignment 1-Image Carousel/carousel.js
+++ JS Assignment 1-Image Carousel/carousel.js
@@ -1,40 +1,55 @@
-//creating left button
+// creating left button
 var arrowLeft = document.createElement("div");
 arrowLeft.className = "arrow";
 arrowLeft.id = "arrow-left";
 
-//creating right button
+// creating right button
 var arrowRight = document.createElement("div");
 arrowRight.className = "arrow";
 arrowRight.id = "arrow-right";
 
-var images = document.querySelectorAll("img");
-len = images.length;
-
 //inserting clearfix
 wrapper = document.querySelector(".carousel-image-wrapper");
-wrapper.style.width = len * images[0].width;
-
 wrapper.classList.add("clearfix");
 
-inserting the buttons
-var container = document.querySelector(".carousel-container");
+var images = document.querySelectorAll("img");
+len = images.length;
+// wrapper.style.width = len * images[0].width;
 
-images[0].insertAdjacentElement("beforebegin", arrowLeft);
-images[len - 1].insertAdjacentElement("afterend", arrowRight);
+// var container = document.querySelector(".carousel-container");
+//inserting the buttons
+// images[0].insertAdjacentElement("beforebegin", arrowLeft);
+// images[len - 1].insertAdjacentElement("afterend", arrowRight);
+wrapper.insertAdjacentElement("beforebegin", arrowLeft);
+wrapper.insertAdjacentElement("beforebegin", arrowRight);
 
-// for (var i = 0; i < len; i++) {
-//   images[i].className = "slide-image";
-// }
+for (var i = 0; i < len; i++) {
+  images[i].classList.add("slide-image");
+}
 
-var count = 0;
+var current = 0;
+var first = 0;
+var last = len - 1;
+
+// arrowLeft = document.querySelector("#btn-left");
+// arrowRight = document.querySelector("#btn-right");
 
 arrowLeft.addEventListener("click", slideLeft);
 arrowRight.addEventListener("click", slideRight);
 
 function slideLeft() {
-  console.log("left");
+  //   console.log("left");
+  current--;
+  console.log(current);
+  wrapper.style.left = images[0].width * current + "px";
+  //   wrapper.style.marginLeft = -images[0].width;
+  //   wrapper.style.marginRight = -images[0].width;
 }
 function slideRight() {
-  console.log("right");
+  //   console.log("right");
+  current++;
+  console.log(current);
+  wrapper.style.left = images[0].width * current + "px";
+  //   wrapper.style.marginRight = images[0].width;
+  //   wrapper.style.marginLeft = -images[0].width;
 }
