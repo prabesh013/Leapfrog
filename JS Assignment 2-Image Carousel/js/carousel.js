9a5bbcc1d456558ca99ebbad1f867a5290f37961
prabesh013 <nprabesh013@gmail.com>
2020-06-29 01:40:00 +0545

    Made multiple instances

--- JS Assignment 2-Image Carousel/js/carousel.js
+++ JS Assignment 2-Image Carousel/js/carousel.js
@@ -1,17 +1,36 @@
-var container = document.querySelector(".carousel-container");
-container.style.display = "none";
+var containerOne = document.querySelector("#container_one");
+var containerTwo = document.querySelector("#container_two");
+var containerThree = document.querySelector("#container_three");
+var containerFour = document.querySelector("#container_four");
 
-function carousel() {
-  //inserting clearfix
-  container.style.display = "block";
-  var wrapper = document.querySelector(".carousel-image-wrapper");
+function Carousel(container, ho = 0, tr = 0) {
+  var wrapper = container.querySelector(".carousel-image-wrapper");
   wrapper.classList.add("clearfix");
 
-  //adding indicators
-  var indicator = document.createElement("div");
-  indicator.classList.add("indicator");
-  wrapper.insertAdjacentElement("afterend", indicator);
+  images = wrapper.querySelectorAll("img");
+  len = images.length;
+  var current = 0;
+  var first = 0;
+  var last = len - 1;
+  var timer;
+  wrapper.style.width = len * images[0].width + "px";
 
+  dots(container, wrapper, len);
+  b = buttons(container, wrapper, current, len);
+  //first dot active
+  makeActive(container, current);
+  timer = setInterval(function () {
+    console.log(container);
+  }, 3000);
+}
+var hold;
+var transition;
+var c1 = new Carousel(containerOne, hold, transition);
+var c2 = new Carousel(containerTwo);
+var c3 = new Carousel(containerThree);
+var c4 = new Carousel(containerFour);
+
+function buttons(c, w, p, l) {
   // creating left button
   var arrowLeft = document.createElement("div");
   arrowLeft.className = "arrow";
@@ -23,104 +42,63 @@ function carousel() {
   arrowRight.id = "arrow-right";
 
   // inserting the buttons
-  wrapper.insertAdjacentElement("beforebegin", arrowLeft);
-  wrapper.insertAdjacentElement("beforebegin", arrowRight);
-
-  //floating the images
-  images = document.querySelectorAll("img");
-  len = images.length;
-  for (var i = 0; i < len; i++) {
-    images[i].classList.add("slide-image");
-  }
-  //making wrapper fit all images
-  wrapper.style.width = len * images[0].width + "px";
-
-  var current = 0;
-  var first = 0;
-  var last = len - 1;
-  var timer;
-
-  //adding dots via JS
-  function addDots() {
-    for (var i = 0; i < len; i++) {
-      var span = document.createElement("span");
-      span.classList.add("dot");
-      document.querySelector(".indicator").appendChild(span);
-    }
-  }
-  addDots();
-
-  //making dots clickable
-  var spans = document.querySelectorAll(".indicator .dot");
-  for (let i = 0; i < len; i++) {
-    spans[i].addEventListener("click", function () {
-      moveSlide(i);
-    });
-  }
-
-  function moveSlide(c) {
-    current = c;
-    makeActive(current);
-    wrapper.style.left = images[0].width * -current + "px";
-  }
-
-  function makeActive(c) {
-    var active = document.querySelector("#active");
-    if (active != null) {
-      active.removeAttribute("id");
-    }
-    spans[c].id = "active";
-  }
-  //first dot active
-  makeActive(current);
+  w.insertAdjacentElement("beforebegin", arrowLeft);
+  w.insertAdjacentElement("beforebegin", arrowRight);
 
   // adding event listeners
   arrowLeft.addEventListener("click", slideLeft);
   arrowRight.addEventListener("click", slideRight);
-
   //reseting current position
-  function checkCurrent(c) {
-    if (c === -1) {
-      return last;
-    } else if (c === len) {
-      return first;
+  function checkCurrent(pointer) {
+    if (pointer === -1) {
+      return l - 1;
+    } else if (pointer === l) {
+      return 0;
     } else {
-      return c;
+      return pointer;
     }
   }
 
   function slideLeft() {
-    current--;
-    current = checkCurrent(current);
-    makeActive(current);
-    wrapper.style.left = images[0].width * -current + "px";
+    p--;
+    p = checkCurrent(p);
+    makeActive(c, p);
+    w.style.left = images[0].width * -p + "px";
   }
   function slideRight() {
-    current++;
-    current = checkCurrent(current);
-    makeActive(current);
-    wrapper.style.left = images[0].width * -current + "px";
+    p++;
+    p = checkCurrent(p);
+    makeActive(c, p);
+    w.style.left = images[0].width * -p + "px";
     // incremtal(current);
   }
+}
+function dots(c, w, l) {
+  //adding indicators
+  var indicator = document.createElement("div");
+  indicator.classList.add("indicator");
+  w.insertAdjacentElement("afterend", indicator);
 
-  function slideShow() {
-    slideRight();
+  for (var i = 0; i < l; i++) {
+    var span = document.createElement("span");
+    span.classList.add("dot");
+    c.querySelector(".indicator").appendChild(span);
   }
-  timer = setInterval(slideShow, 3000);
-
-  // function incremtal(current) {
-  //   clearInterval(timer);
-  //   var left = parseInt(wrapper.style.left);
-  //   var total = images[0].width * -current;
-  //   var sliding = setInterval(function () {
-  //     left = left + 10;
-  //     wrapper.style.left = left + "px";
-  //     if (-left % images[0].width === 0) {
-  //       clearInterval(sliding);
-  //     }
-  //     slideShow();
-  //   }, 1000 / 60);
-  // }
+  var spans = c.querySelectorAll(".indicator .dot");
+  for (let i = 0; i < l; i++) {
+    spans[i].addEventListener("click", function () {
+      current = i;
+      makeActive(c, current);
+      w.style.left = images[0].width * -current + "px";
+    });
+  }
 }
 
-var carousel1 = new carousel();
+function makeActive(ctner, c) {
+  var spans = ctner.querySelectorAll(".indicator .dot");
+  var active = ctner.querySelector("#active");
+  if (active != null) {
+    active.removeAttribute("id");
+  }
+  spans[c].id = "active";
+}
