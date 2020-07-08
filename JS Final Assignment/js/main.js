c4c021b973284e4aab928940d67a96e9f3a4d5d2
prabesh013 <nprabesh013@gmail.com>
2020-07-08 18:37:42 +0545

    Changes to Weights.js and Canvas data

--- JS Final Assignment/js/main.js
+++ JS Final Assignment/js/main.js
@@ -1,3 +1,4 @@
+//Document Object Model Selector Constants
 const container = document.querySelector(".container");
 const main = document.querySelector(".main");
 const expressionArea = document.querySelector(".expression");
@@ -6,6 +7,7 @@ const drawing = document.querySelector(".drawingArea");
 const canvas = document.querySelector("#drawing-sheet");
 const ctx = canvas.getContext("2d");
 
+//clears the canvas
 const button = document.querySelector(".clear-btn");
 button.addEventListener("click", function () {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -14,30 +16,37 @@ button.addEventListener("click", function () {
   playing = false;
 });
 
+//drawing Logic
+
+//initial position
 let currentX = 0;
 let currentY = 0;
 let previousX = 0;
 let previousY = 0;
 
+//until clicked n the canvas area, shouldn't draw anything even if mouse moves
 let playing = false;
 
+//basic styling for canvas
 ctx.fillStyle = "#00000";
 ctx.fillRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-
 ctx.strokeStyle = "#ffffff";
 ctx.lineWidth = 7;
-
 ctx.lineJoin = "round";
 
+//only when mouse is clicked in the canvas get current positions and drawing starts
 canvas.addEventListener("mousedown", function (e) {
   playing = true;
   currentX = e.clientX - canvas.offsetLeft;
   currentY = e.clientY - canvas.offsetTop;
 });
+
+//drawing stop as soon as mouse is "unclicked"
 canvas.addEventListener("mouseup", function (e) {
   playing = false;
 });
 
+//this draws the line from previous position to current position
 canvas.addEventListener("mousemove", function (e) {
   if (playing) {
     previousX = currentX;
@@ -61,8 +70,8 @@ function randInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-let first = randInt(0, 6);
-let second = randInt(0, 5);
+let first = randInt(0, 5);
+let second = randInt(0, 4);
 
 let answer = first + second;
 let expression = `${first} + ${second}`;
@@ -75,6 +84,14 @@ expressionArea.appendChild(p);
 const viewImage = document.querySelector(".view-image");
 let image = document.createElement("img");
 const playButton = document.querySelector(".play-btn");
+
+const imageCanvas = document.querySelector("#another");
+const imageCanvasContext = imageCanvas.getContext("2d");
+
+var pixel = null;
+var data = null;
+var imageRComponent = [];
+
 playButton.addEventListener("click", function () {
   const dataURL = canvas.toDataURL();
   image.style.display = "inline-block";
@@ -82,4 +99,20 @@ playButton.addEventListener("click", function () {
   image.style.height = "28px";
   image.style.width = "28px";
   viewImage.appendChild(image);
+  imageCanvasContext.drawImage(image, 0, 0, 28, 28);
+  viewImage.appendChild(imageCanvas);
+  // var myImageData = imageCanvasContext.createImageData(width, height);
+  pixel = imageCanvasContext.getImageData(0, 0, 28, 28);
+  data = pixel.data;
+  rComponent();
+  logic(imageRComponent);
 });
+
+var rComponent = function () {
+  let counter = 0;
+  for (var i = 0; i < data.length; i += 4) {
+    imageRComponent[counter] = data[i];
+    counter++;
+  }
+  console.log(counter);
+};
