232d50ba1022810bf5cb4733b73fbf058797bac4
prabesh013 <nprabesh013@gmail.com>
2020-07-15 19:44:47 +0545

    Works

--- JS Final Assignment/js/main.js
+++ JS Final Assignment/js/main.js
@@ -1,1 +1,38 @@
+//generating random number
+function randInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
+let operators = ["+", "-", "*", "/"];
+
+function addExpression(expression) {
+  let p = document.createElement("p");
+  let text = document.createTextNode(expression);
+  p.appendChild(text);
+  expressionArea.appendChild(p);
+}
+function checkAnswer(answer) {}
+
+function easy() {
+  let first = randInt(0, 5);
+  let second = randInt(0, 4);
+  let operator = operators[0];
+  //   let timer =
+
+  let expression = `${first} ${operator} ${second}`;
+  let answer = first + second;
+
+  addExpression(expression);
+  checkAnswer(answer);
+}
+function medium() {
+  let first = randInt(0, 5);
+  let second = randInt(0, 4);
+  let operator = operators[randInt(0, 3)];
+
+  let expression = `${first} ${operator} ${second}`;
+  let answer = first + second;
+
+  addExpression(expression);
+  checkAnswer(answer);
+}
